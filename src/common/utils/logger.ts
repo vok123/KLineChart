8d1d32192cc324a760fb87c3bc2d09daeb266251
vok123 <512855971@qq.com>
2025-05-06 14:57:58 +0800

    Update logger.ts

--- src/common/utils/logger.ts
+++ src/common/utils/logger.ts
@@ -42,12 +42,12 @@ export function logError (api: string, invalidParam: string, append?: string): v
 }
 
 export function logTag (): void {
-  log(
-    '%c❤️ Welcome to klinecharts. Version is __VERSION__',
-    'border-radius:4px;border:dashed 1px #1677FF;line-height:70px;padding:0 20px;margin:16px 0;font-size:14px;color:#1677FF;',
-    '',
-    '',
-    '',
-    ''
-  )
+  // log(
+  //   '%c❤️ Welcome to klinecharts. Version is __VERSION__',
+  //   'border-radius:4px;border:dashed 1px #1677FF;line-height:70px;padding:0 20px;margin:16px 0;font-size:14px;color:#1677FF;',
+  //   '',
+  //   '',
+  //   '',
+  //   ''
+  // )
 }
